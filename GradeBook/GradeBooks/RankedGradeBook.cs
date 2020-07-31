192828395947eb75a55bffe171d1f6e367ff5ad3
Berry <hayden.berry@sfbli.com>
2020-07-31 11:04:04 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,5 +26,17 @@ namespace GradeBook.GradeBooks
 
             return 'F';
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+
+        }
     }
 }
