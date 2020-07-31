e1df050100d4fe8eb75c514daf5fc3d71ef5c5a5
Berry <hayden.berry@sfbli.com>
2020-07-31 11:00:38 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,20 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            if(averageGrade >= 80.0) { return 'A'; }
+            else if(averageGrade >= 60.0) { return 'B'; }
+            else if(averageGrade >= 40.0) { return 'C'; }
+            else if(averageGrade >= 20.0) { return 'D'; }
+
+            return 'F';
+        }
     }
 }
